9592f4d5127db879768b8e6239da5198a0550740
sheila-serafim <sheila.serafim@gmail.com>
2022-06-20 17:47:09 -0300

    adding test file

---
+++ cmd/server/controllers/products_test.go
@@ -0,0 +1,11 @@
+package controllers_test
+
+import (
+	"testing"
+
+	"github.com/emidioreb/mercado-fresco-lerigophers/internal/products/mocks"
+)
+
+func Test_Get_Product_OK(t *testing.T) {
+	mockedService := new(mocks.Service)
+}
