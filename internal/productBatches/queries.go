a3a72159353706a47189cc5e0a4b18cc95218e78
bulhoes1998 <lucas.bulhoes@mercadolivre.com>
2022-07-05 16:12:36 -0300

    implementing productBatches feature

--- internal/productBatches/queries.go
+++ internal/productBatches/queries.go
@@ -7,8 +7,8 @@ var (
 
 	queryGetReportOne = `SELECT s.id as section_id, s.section_number, count(*) as sections_count
 	FROM sections s
-	JOIN product_batches pb ON s.id = pb.section_id WHERE id = ? GROUP BY s.id, s.section_number;`
+	JOIN product_batches pb ON s.id = pb.section_id WHERE s.id = ? GROUP BY s.id, s.section_number;`
 
-	queryCreateProductBatch = `INSERT INTO product_batches (batch_number, current_quatity, current_temperature, initial_quantity, manufacturing_hour, minimum_temperature, product_id, section_id, due_date, manufacturing_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
-	queryGetOneProductBatch = `SELECT * FROM product_batches WHERE id = ?`
+	queryCreateProductBatch = `INSERT INTO product_batches (batch_number, current_quatity, current_temperature, initial_quantity, manufacturing_hour, minimum_temperature, product_id, section_id, due_date, manufacturing_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
+	queryGetOneProductBatch = `SELECT * FROM product_batches WHERE id = ?;`
 )
